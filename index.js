5c4d63f3705f0819582af35aed816d287f9ef556
Bassman2 <ralf.beckers@hotmail.com>
2021-10-30 18:01:30 +0200

    work

--- index.js
+++ index.js
@@ -8,10 +8,17 @@ async function run() {
 
 		const eol = "\r\n";	
 		
-		const prjTypeFolder = "{2150E333-8FDC-42A3-9474-1A3956D46DE8}";
-		const prjTypeFwk = "{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}";
-		const prjTypeCore = "{9A19103F-16F7-4668-BE54-9A1E7A4F7556}";
-		const prjTypeShare = "{D954291E-2A0B-460D-934E-DC6B0785DB48}";
+		const PrjType = {
+			Folder: 1,
+			Framework: 2,
+			Core: 3,
+			Share: 4
+		};
+		
+		const prjUuidFolder = "{2150E333-8FDC-42A3-9474-1A3956D46DE8}";
+		const prjUuidFwk = "{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}";
+		const prjUuidCore = "{9A19103F-16F7-4668-BE54-9A1E7A4F7556}";
+		const prjUuidShare = "{D954291E-2A0B-460D-934E-DC6B0785DB48}";
 		
 		const solutionName = core.getInput('name');
         const solutionProjects = core.getInput('projects').split(',').map(p => p.trim());
@@ -23,11 +30,34 @@ async function run() {
 
         var projects = [];
 		for (const solutionProject of solutionProjects) { 
+			var ext = path.extname(solutionProject);
+			var prjType = PrjType.Folder
+			var prjUuid = '';
+			switch (ext) {
+				case '.shproj':
+					prjType = PrjType.Core;
+					prjUuid = prjUuidCore;
+					break;
+				case '.csproj':
+					prjType = PrjType.Share;
+					prjUuid = prjUuidShare;
+					break;
+				case '':
+					prjType = PrjType.Folder;
+					prjUuid = prjUuidFolder
+					break;
+				default:
+					core.setFailed(`${ext} not defined`);
+					break;
+			}
+					
+		
 			let p = {
+				"type" : prjType,
+				"typeid" : prjUuid,
 				"name" : path.basename(solutionProject).split('.').slice(0, -1).join('.'),
 				"path" : solutionProject,
-				"type" : "",
-				"id" : uuid.v4()
+				"prjid" : uuid.v4()
 			}
 			projects.push(p); 
 		}
@@ -38,7 +68,7 @@ async function run() {
 		writer.write('Microsoft Visual Studio Solution File, Format Version 12.00' + eol);
 
         for (const p of projects) { 
-			writer.write(`Project("${p.type}") = "${p.name}", "${p.path}", "${p.id}"` + eol);
+			writer.write(`Project("${p.typeid}") = "${p.name}", "${p.path}", "${p.prjid}"` + eol);
 			writer.write('EndProject' + eol);
 		}
 		
