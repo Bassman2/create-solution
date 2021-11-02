cb821f254c72bf2c27cfb1a3fd8acbf6dfa45b52
bs <ralf.beckers@elektrobit.com>
2021-11-02 18:08:51 +0100

    update

---
+++ Tests/UnitTestProject4/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Elektrobit")]
+[assembly: AssemblyProduct("UnitTestProject4")]
+[assembly: AssemblyCopyright("Copyright © Elektrobit 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ef1911ba-6fbb-4907-b248-3252c9d7a622")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- index.js
+++ index.js
@@ -3,26 +3,41 @@ const core = require('@actions/core'),
       path = require('path'),
 	  fs = require('fs');
 
+const eol = "\r\n";	
+		
+const PrjType = {
+	Folder: 1,
+	CSFramework: 2,
+	CSCore: 3,
+	VBFramework: 4,
+	VBCore: 5,
+	Share: 6,
+	VisualC: 7,
+	SandcastleHelpFileBuilder: 8,
+	Setup: 9,
+	FS: 10
+};
+
+// https://newbedev.com/visual-studio-project-type-guids
+
+const projectGuidFolder = '{2150E333-8FDC-42A3-9474-1A3956D46DE8}';
+const projectGuidVC = '{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}';
+const projectGuidVBFramework = '{F184B08F-C81C-45F6-A57F-5ABD9991F28F}';
+const projectGuidVBCore = '{778DAE3C-4631-46EA-AA77-85C1314464D9}';
+const projectGuidCSFramework = '{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}';
+const projectGuidCSCore = '{9A19103F-16F7-4668-BE54-9A1E7A4F7556}';
+const projectGuidShare = '{D954291E-2A0B-460D-934E-DC6B0785DB48}';
+const projectGuidSandcastle = '{7CF6DF6D-3B04-46F8-A40B-537D21BCA0B4}';
+const projectGuidFS = '{6EC3EE1D-3C4E-46DD-8F32-0CC8E7565705}';
+const projectGuidSetup = '{54435603-DBB4-11D2-8724-00A0C9A8B90C}';
+		
 async function run() {
     try {
-
-		const eol = "\r\n";	
-		
-		const PrjType = {
-			Folder: 1,
-			Framework: 2,
-			Core: 3,
-			Share: 4
-		};
-		
-		const prjUuidFolder = "{2150E333-8FDC-42A3-9474-1A3956D46DE8}";
-		const prjUuidFwk = "{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}";
-		const prjUuidCore = "{9A19103F-16F7-4668-BE54-9A1E7A4F7556}";
-		const prjUuidShare = "{D954291E-2A0B-460D-934E-DC6B0785DB48}";
+		const solutionGuid = `{${uuid.v4()}}`;
 		
 		const solutionName = core.getInput('name');
         const solutionProjects = core.getInput('projects').split(',').map(p => p.trim());
-        const solutionConfigs = core.getInput('configurations').split(',').map(p => p.trim());
+        const solutionConfigs = (core.getInput('configurations') || 'Debug|Any CPU,Release|Any CPU').split(',').map(p => p.trim());
 		
         // output infos
         console.log('name: ', solutionName);
@@ -34,41 +49,94 @@ async function run() {
 			var ext = path.extname(solutionProject);
 			var prjType = PrjType.Folder
 			var prjUuid = '';
+			var prjConf = false;
+			var prjShare = false;
 			switch (ext) {
-				case '.shproj':
-					prjType = PrjType.Share;
-					prjUuid = prjUuidShare;
-					break;
+				// C# project
 				case '.csproj':
+					prjConf = true;
 					if (IfFileStartsWith(solutionProject, '<Project')) {
-						prjType = PrjType.Core;
-						prjUuid = prjUuidCore;
+						prjType = PrjType.CSCore;
+						prjUuid = projectGuidCSCore;
 					} else {
-						prjType = PrjType.Framework;
-						prjUuid = prjUuidFwk;						
+						prjType = PrjType.CSFramework;
+						prjUuid = projectGuidCSFramework;						
 					}
 					break;
-				case '':
-					prjType = PrjType.Folder;
-					prjUuid = prjUuidFolder
+
+				// F# project
+				case 'fsproj':
+					prjConf = true;
+					prjType = PrjType.FS;
+					prjUuid = projectGuidFS;
 					break;
+
+				// shared project
+				case '.shproj':
+					prjConf = false;
+					prjShare = true;
+					prjType = PrjType.Share;
+					prjUuid = projectGuidShare;
+					break;
+
+				// Visual Basic project
+				case '.vbproj':
+					prjConf = true;
+					if (IfFileStartsWith(solutionProject, '<Project')) {
+						prjType = PrjType.VBCore;
+						prjUuid = projectGuidVBCore
+					} else {
+						prjType = PrjType.VBFramework;
+						prjUuid = projectGuidVBFramework;
+					}
+					break;
+					
+				// C++ project
+				case '.vcxproj':
+					prjConf = true;
+					prjType = PrjType.VisualC;
+					prjUuid = projectGuidVC;
+					break;
+					
+				// Setup project
+				case '.vdproj':
+					prjConf = true;
+					prjType = PrjType.Setup;
+					prjUuid = projectGuidSetup;
+					break;
+					
+				// Sandcastle Help File Builder project
+				case '.shfbproj':
+					prjConf = true;
+					prjType = PrjType.SandcastleHelpFileBuilder;
+					prjUuid = projectGuidSandcastle
+					break;
+					
+				// Solution Folder	
+				case '':
+					prjConf = false;
+					prjType = PrjType.Folder;
+					prjUuid = projectGuidFolder;
+					break;
+					
+				// unknown
 				default:
 					core.setFailed(`${ext} not defined`);
 					break;
-			}
-					
+			}					
 		
 			let p = {
-				"type" : prjType,
+				"type": prjType,
+				"conf": prjConf,
+				"share": prjShare,
 				"typeid" : prjUuid,
 				"name" : path.basename(solutionProject).split('.').slice(0, -1).join('.'),
 				"path" : solutionProject,
-				"prjid" : `{${uuid.v4()}}`
+				"prjid": `{${uuid.v4()}}`,
+				"items": solutionProject.substr(0, solutionProject.lastIndexOf(".")) + '".projitems'
 			}
 			projects.push(p); 
 		}
-
-
         
 		let writer = fs.createWriteStream(solutionName + '.sln');
 		writer.write('Microsoft Visual Studio Solution File, Format Version 12.00' + eol);
@@ -77,8 +145,17 @@ async function run() {
 			writer.write(`Project("${p.typeid}") = "${p.name}", "${p.path}", "${p.prjid}"` + eol);
 			writer.write('EndProject' + eol);
 		}
-		
+				
 		writer.write('Global' + eol);
+
+		if (projects.some(p => p.share)) {
+			writer.write('    GlobalSection(SharedMSBuildProjectFiles) = preSolution' + eol);
+			for (const p of projects.filter(p => p.share)) {
+				writer.write(`    ${items}*${prjid}*SharedItemsImports = 13` + eol);
+			}
+			writer.write('    EndGlobalSection' + eol);
+		}
+
 		writer.write('    GlobalSection(SolutionConfigurationPlatforms) = preSolution' + eol);
 		for (const c of solutionConfigs) {
 			writer.write(`        ${c} = ${c}` + eol);
@@ -86,11 +163,10 @@ async function run() {
 		writer.write('    EndGlobalSection' + eol);
 
 		writer.write('    GlobalSection(ProjectConfigurationPlatforms) = postSolution' + eol);
-		for (const p of projects) {
+		for (const p of projects.filter(p => p.conf)) {
 			for (const c of solutionConfigs) {
 				writer.write(`        ${p.prjid}.${c}.ActiveCfg = ${c}` + eol);
-				writer.write(`        ${p.prjid}.${c}.Build.0 = ${c}` + eol);
-				
+				writer.write(`        ${p.prjid}.${c}.Build.0 = ${c}` + eol);				
 			}
 		}
 		writer.write('    EndGlobalSection' + eol);
@@ -98,15 +174,11 @@ async function run() {
 		writer.write('    GlobalSection(SolutionProperties) = preSolution' + eol);
 		writer.write('        HideSolutionNode = FALSE' + eol);
 		writer.write('    EndGlobalSection' + eol);
+		
 		writer.write('    GlobalSection(ExtensibilityGlobals) = postSolution' + eol);
-		writer.write('        SolutionGuid = {BB157E78-9E24-4CC2-98B9-FA3D414F9318}' + eol);
+		writer.write(`        SolutionGuid = ${solutionGuid}` + eol);
 		writer.write('    EndGlobalSection' + eol);
 		writer.write('EndGlobal' + eol);
-
-
-
-		//writer.write(`xxx ${name}`);
-
 
 		writer.end();
 
